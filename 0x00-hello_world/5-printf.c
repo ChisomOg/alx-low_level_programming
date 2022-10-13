b73c4ecbc16b1bf36eafad8d9c0aa443c4896694
chisom <chisom.ogueri1@gmail.com>
2022-10-13 13:36:46 -0700

    6

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry for program
+ *
+ * Return: 0 if no error and non zero if error
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+	return (0);
+}
