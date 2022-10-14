f36b5553f01ce6fd1313b87d6c035ae077020806
chisom <chisom.ogueri1@gmail.com>
2022-10-14 16:16:09 -0700

    r6

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,21 +4,18 @@
  * main-program entry point.
  *
  * Return:0 no error, non zero if error.
- *
  */
 
 int main(void)
-
 {
 	char i;
 	char j;
-				
+
 	for (i = 'a'; i <= 'z'; i++)
 	{
 		putchar(i);
 	}
 	for (j = 'A'; j <= 'Z'; j++)
-
 	{
 		putchar(j);
 	}
