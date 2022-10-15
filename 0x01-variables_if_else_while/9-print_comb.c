73c2bee899e9530f4227c927dc4dd9f3f6ceb01d
chisom <chisom.ogueri1@gmail.com>
2022-10-14 17:16:52 -0700

    r16

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/** 
+ * main - entry for code
+ *
+ * Return: return 0 if success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+		if (i == '9')
+		{
+			break;
+		}
+		else
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
