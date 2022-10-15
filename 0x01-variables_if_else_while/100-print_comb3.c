c07ad2a3a61c5a23928b180196d6c561ac08b712
chisom <chisom.ogueri1@gmail.com>
2022-10-14 17:31:39 -0700

    r18

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/** 
+ * main - entry for code
+ *
+ * Return: if 0 a success
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			if (i != j)
+			{ 
+				putchar(i);
+				putchar(j);
+			}
+			if (i == j)
+			{
+				continue;
+			}
+			if (i == '8' && j == '9')
+			{
+				break;
+			}
+			else 
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		putchar('\n');
+		return (0);
+	}
