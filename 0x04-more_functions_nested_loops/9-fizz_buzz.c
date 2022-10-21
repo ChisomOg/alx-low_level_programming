6a0f54a8cdf2ac2f8d8d6047645f6b7fab8c8c54
chisom <chisom.ogueri1@gmail.com>
2022-10-21 14:12:08 -0700

    i13

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,14 +12,18 @@ int main(void)
 	for (num = 1; num <= 100; ++num)
 	{
 		if (num % 3 == 0)
-			printf("Fizz ");
+			printf("Fizz");
 		else if (num % 5 == 0)
-		       	printf("Buzz ");
+			printf("Buzz");
 		else if (num % 15 == 0)
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		else
-			printf("%d ", num);
+			printf("%d", num);
+
+		if (num != 100)
+			printf(" ");
+		else
+			printf("\n");
 	}
-	printf("\n");
 	return (0);
 }
