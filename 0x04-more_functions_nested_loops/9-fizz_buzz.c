5964649d35bcde912cc8845c033c9e7ec111152f
chisom <chisom.ogueri1@gmail.com>
2022-10-21 13:15:38 -0700

    i1

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,7 +14,7 @@ int main(void)
 		if (num % 3 == 0 && !(num % 5 == 0))
 			printf("Fizz");
 		else if (num % 5 == 0 && !(num % 3 == 0))
-		       printf("Buzz");
+		       	printf("Buzz");
 		else if (num % 3 == 0 && num % 5 == 0)
 			printf("FizzBuzz");
 		else
