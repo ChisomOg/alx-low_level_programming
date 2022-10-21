77b7fe4f35c59770d261cb64137ae3a6ffcad324
chisom <chisom.ogueri1@gmail.com>
2022-10-21 14:42:50 -0700

    i21

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include"main.h"
+
+/**
+ * main - test function
+ *
+ * Retur: always success for 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 98 % 2;
+	positive_or_negative(i);
+
+	return (0);
+}
