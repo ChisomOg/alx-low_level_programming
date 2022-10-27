dc74eb8ff3df3fc4323746b77c08ebabb873d484
chisom <chisom.ogueri1@gmail.com>
2022-10-27 11:01:41 -0700

    s3

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * infinite_add - C function adds two numbers
+ *
+ * @n1: first number
+ * @n2: second number
+ * @r: store result
+ * @size_r: size of buffer
+ * Return: returns pointer to result
+ */
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+}
+
+/**
+ * add_strings - add the numbers stored in two strings
+ * @n1: string containing first number
+ * @n2: second number
+ * @r: buffer to store result
+ * @r_index: buffer to store
+ * Return: if r can store two sums
+ */
+char *add_strings(char *n1, char *n2, char *r, int r_index)
+{
+	int num, tens = 0;
+
+	for (; *n1 &&*n2; n1--, n2--, r_index--)
+	{
+		num = (*n1 - '0') + (*n2 - '0');
+		num += tens;
+		*( r + r_index) = (num % 10) + '0';
+		tens = num / 10;
+	}
+
+	for (; *n1; n1--; r_index++)
+	{
+		num = *(n1 - '0') + tens;
+		*(r + r_index) = (num % 10) + '0';
+		tens = num / 10;
+	}
+
+	for (; *n2; n2--; r_index--)
+	{
+		num = (*n2 - '0') + tens;
+		*(r + r_index) = (num % 10) + '0';
+		tens = num / 10;
+	}
+}
