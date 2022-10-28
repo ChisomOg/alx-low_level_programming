0d768ad72eabc0d43b2b5002882cd34e5f9483bb
chisom <chisom.ogueri1@gmail.com>
2022-10-27 19:55:13 -0700

    s10

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,13 +11,15 @@ char *cap_string(char *str)
 {
 	int i = 0;
 
-	while (str[++i])
+	while (str[i] != '\0')
 	{
-		while (!(str[i] >= 'a' && str[i] <= 'z'))
-			i++;
-		if (str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' || str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' || str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' || str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' || str[i - 1] == '}')
+		if ((str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' || str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' || str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' || str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' || str[i - 1] == '}') && (s[i] >= 'a' && s[i] <= 'z'))
 			str[i] -= 32;
+		else if (( str[0] >= 97 && str[0] <= 122))
+			str[0] -= 32;
+		else 
+			str[i] = str[i];
+		i++;
 	}
-
 	return (str);
 }
