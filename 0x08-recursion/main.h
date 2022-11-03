34ec48d66ceb159fcbc4434731532027f9c3538f
chisom <chisom.ogueri1@gmail.com>
2022-11-03 08:14:58 -0700

    p11

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,8 +8,12 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
+int _sqrt(int n, int i);
 int is_prime_number(int n);
+int is_prime(int n, int start);
 int find_strlen(char *s);
+int is_palindrome(char *s);
+int p1(char *s, int l);
 int check_palindrome(char *s, int len, int index);
 
 #endif
