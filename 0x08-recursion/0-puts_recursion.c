3b48d5c0eda029693385d5202f9b01fe8815f4d2
chisom <chisom.ogueri1@gmail.com>
2022-11-03 06:42:38 -0700

    p6

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * _puts_recursion - prints a string followed by a new line
  * @s: character to be printed
@@ -11,7 +11,7 @@ void _puts_recursion(char *s)
 	if (*s != '\0')
 	{
 		_putchar(*s);
-		_point_recursion(s + 1);
+		_puts_recursion(s + 1);
 	}
 	else
 	{
