ee09568883e52a765da16522fa20cced409f7140
chisom <chisom.ogueri1@gmail.com>
2022-11-03 06:22:28 -0700

    p1

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+
+void _puts_recursion(char *s);
+void _print_re_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int find_strlen(char *s);
+int check_palindrome(char *s, int len, nt index);
+
+#endif
